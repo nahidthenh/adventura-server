66045c643287aa8f5c403cd9d145d8c8cde2f5bb
nahidthenh <nhgroup18@gmail.com>
2023-07-07 15:12:41 +0600

    hotels api setup

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 
+const hotels = require('./data/hotels.json')
+
 app.get('/', (req, res) => {
     res.send('Adventura Server Is Running !!! ')
 })
+
+
+app.get('/hotels', (req, res) => {
+    res.send(hotels)
+})
+
 
 app.listen(port, () => {
     console.log(`Adventura Server Running On Port ${port}`)
